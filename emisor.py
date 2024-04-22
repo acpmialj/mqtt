50a097d3cc0d48bc2415b36501d27219cf692ace
Jose Miguel-Alonso <106067594+acpmialj@users.noreply.github.com>
2024-04-22 17:52:42 +0200

    Update emisor.py

--- emisor.py
+++ emisor.py
@@ -1,10 +1,4 @@
-$ python3
-Python 3.10.6 (main, Aug 10 2022, 11:40:04) [GCC 11.3.0] on linux
-Type "help", "copyright", "credits" or "license" for more information.
->>> import paho.mqtt.client as mqtt
->>> broker_address="172.17.0.2"
->>> client.connect(broker_address)
-0
->>> client.publish("house/main-ligth", "OFF")
-<paho.mqtt.client.MQTTMessageInfo object at 0x7f435d60af20>
->>> 
+import paho.mqtt.client as mqtt
+client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+client.connect("mosquitto")
+client.publish("house/main-ligth", "OFF")
